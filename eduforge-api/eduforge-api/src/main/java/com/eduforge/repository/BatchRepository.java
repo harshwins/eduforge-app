0bdf948f94e3abaecc305d48ad7d9d80f92218f6
Harsh Gohil <harshgohil@Harshs-MacBook-Pro.local>
2025-05-09 06:08:07 +0530

    Remove nested eduforge-api directory

--- eduforge-api/eduforge-api/src/main/java/com/eduforge/repository/BatchRepository.java
+++
@@ -1,7 +0,0 @@
-// src/main/java/com/eduforge/repository/BatchRepository.java
-package com.eduforge.repository;
-
-import com.eduforge.model.Batch;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BatchRepository extends JpaRepository<Batch,Integer> {}
